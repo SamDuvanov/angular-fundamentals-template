c176019c88f6bd3415d8ed162f522191ef7b874a
Semen Duvanov <samduvanov@gmail.com>
2025-05-12 17:29:20 +0300

    Implementing Course card.

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  title: string;
+  description: string;
+  creationDate: Date;
+  duration: number;
+  authors: string[];
+  @Input() isEditable: boolean;
+  
+  @Output() showCourse = new EventEmitter<void>();
+  @Output() editCourse = new EventEmitter<void>();
+  @Output() deleteCourse = new EventEmitter<void>();
+
+  constructor() {
+    this.title = '';
+    this.description = '';
+    this.creationDate = new Date();
+    this.duration = 0;
+    this.authors = [];
+    this.isEditable = true;
+  }
+
+  onShowCourse()
+  {
+    this.showCourse.emit();
+  }
+
+  onEditCourse()
+  {
+    this.editCourse.emit();
+  }
+
+  onDeleteCourse()
+  {
+    this.deleteCourse.emit();
+  }
+}
